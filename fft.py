3c2c18e13cc490b33e998ccd90b6d9752da378fa
bonsaiiV <dominik.riemer@udo.edu>
2022-05-11 13:43:16 +0200

    added code for input generation

--- fft.py
+++ fft.py
@@ -1,9 +1,22 @@
 import numpy as np
 from cmath import exp, pi, log
+from operator import add
+from random import random
+
+def get_sin(T, samples):
+    return [5*np.sin(np.pi * y /T) for y in range(samples)]
+
+def get_cos(T, samples):
+    return [5*np.cos(np.pi * y /T) for y in range(samples)]
 
 
-#x = [1, 4, 9, 16, -1, -4, -9, -16, 1, 4, 9, 16, -1, -4, -9, -16, 1, 4, 9, 16, -1, -4, -9, -16, 1, 4, 9, 16, -1, -4, -9, -16]
-x = [y for y in range(0,(2<<15))]
+x = [1, 4, 9, 16, -1, -4, -9, -16, 1, 4, 9, 16, -1, -4, -9, -16]#, 1, 4, 9, 16, -1, -4, -9, -16, 1, 4, 9, 16, -1, -4, -9, -16]
+N = 1<<15
+T1 = 1<<13
+#x = [y for y in range(0,N)]
+#x = get_sin(T1, N)
+#x = get_cos(T1, N)
+#x = list( map(add , map(add, get_sin( 16, N), get_cos( 32, N)), [random()-0.5 for y in  range(N)] ) )
 
 def pow(b, e):
     return exp(log(b)*e)
@@ -20,7 +33,13 @@ def get(index, list):
     if index >= len(list): return 0
     return list[index]
 
-def radix2_fft(input):
+def calculate_unitroot1(elements_per_block, k):
+    return exp((-2j * pi)*k/elements_per_block)
+
+def calculate_unitroot2(elements_per_block, k):
+    return np.cos(2*pi*k/elements_per_block) + np.sin(-2*pi*k/elements_per_block)*1j
+
+def radix2_fft(input, calculate_unitroot):
     n = 0
     divisor = len(input)
     one_more = 0
@@ -39,33 +58,41 @@ def radix2_fft(input):
         output.append(get(x_even, input) - get(x_odd, input))
     
     for layer in range(2, n+1):
-        unitroot = exp((-2j * pi)/(1 << (layer)))
+        #unitroot = exp((-2j * pi)/(1 << (layer)))
         elements_per_block = 1 << (layer)
         for block in range(1 << (n-layer)):
             for k in range(elements_per_block >> 1):
             
                
-                tmp = pow(unitroot, k)
+                unitroot = calculate_unitroot(elements_per_block, k)
                 k_even = (block * elements_per_block)+k
                 k_odd = k_even+(elements_per_block >> 1)
-                tmp = tmp * output[k_odd]
+                tmp = unitroot * output[k_odd]
                 output[k_odd] = output[k_even] - tmp
                 output[k_even] = output[k_even] + tmp
 
 
     return output
 
-precision = 0
+precision = 6
 def compare_ctuple(tuple):
     return ( round(tuple[0].real, precision) == round(tuple[1].real, precision) 
         and  round(tuple[0].imag, precision) == round(tuple[1].imag, precision) )
-output= radix2_fft(x)
-results =  [(not compare_ctuple(tuple)) for tuple in zip(np.fft.fft(x), output)]
-output2 = np.fft.fft(x)
-for i in range(len(output)):
-    if(results[i]):
-        print(output2[i])        
-        print(round(output[i].real, precision)+round(output[i].imag, precision)*1j, "\n")
-#print(list(map(lambda y : round(y.real, precision)+round(y.imag, precision)*1j ,output)))
-e_rate = np.divide(np.sum(results), len(output))
-print(e_rate)
+
+output1 = radix2_fft(x, calculate_unitroot1)
+output2 = radix2_fft(x, calculate_unitroot2)
+expected_values = np.fft.fft(x)
+results1 =  [(not compare_ctuple(tuple)) for tuple in zip(expected_values, output1)]
+results2 =  [(not compare_ctuple(tuple)) for tuple in zip(expected_values, output2)]
+
+for i in range(len(output1)):
+    if(results1[i]):
+        print(expected_values[i])        
+        print(round(output1[i].real, 9)+round(output1[i].imag, 9)*1j, "\n")
+print(list(map(lambda y : round(y.real, precision)+round(y.imag, precision)*1j ,output1)))
+e_rate1 = np.divide(np.sum(results1), len(output1))
+e_rate2 = np.divide(np.sum(results2), len(output2))
+print(x)
+print("error rate with polarcoordinates:",e_rate1)
+print("error rate with cos and sin:",e_rate2)
+print("with precision", precision)
