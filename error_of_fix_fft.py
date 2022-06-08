-added comandline parameters to fix_fft -removed independent fix and normalize option and made them mandatory -added result calculation for multiple bursts

error_of_fix_fft.py
@@ -2,32 +2,84 @@ from subprocess import check_output
 import numpy as np 
 from matplotlib import pyplot as plt
 
-plot_for_bits = 0
+plot_error = 0
+plot_for_bursts = 1
 
+def print_float(s):
+    print(s+", ")
+    return float(s)
 
-if plot_for_bits:
-    input_bits = range(16, 33)
-    titles = ["absolute errors for sensor 1","absolute errors for sensor 2", "relative errors for sensor 1", "relative errors for sensor 2"]
+if plot_error:
+    input_bits = range(14, 33)
+    twiddle_bits = [12, 14, 16, 28]
+    #titles = ["absolute error"]
+elif plot_for_bursts:
+    input_bursts = range(10, 200, 30)
 else:
-    input_bits =[18, 20, 22, 24]
+    input_bits = range(16,25,2)
 output_list = []
-for amount_of_bits in input_bits:
-    output = bytes.decode(check_output(["./fix_fft", str(amount_of_bits), "SNS_Test_data_ampl.dat"])).split()
-    output = list(map(float ,output))
-    output_list.append(output)
-
-if(plot_for_bits):
-    for i in range(0,len(output)):
-        output_for_plot = np.array(list(map(lambda x : x[i], output_list)))
-        plt.subplot(2,2,i+1)
-        plt.title(titles[i])  
-        plt.plot(np.array(input_bits),output_for_plot)
+if plot_error:
+    for twiddle_index in range(len(twiddle_bits)):
+        output_list.append([])
+        for amount_of_bits in input_bits:
+            output = bytes.decode(check_output(
+                ["./fix_fft",
+                "-b",
+                str(twiddle_bits[twiddle_index]),
+                "-t",
+                str(amount_of_bits),
+                "-e",
+                "SNS_Test_data_long_500.dat"
+                ])).split()
+            output = list(map(float ,output))
+            output_list[twiddle_index].append(output)
+elif plot_for_bursts:
+    for amount_of_bursts in input_bursts:
+        output = bytes.decode(check_output(
+            ["./fix_fft",
+            "-n",
+            str(amount_of_bursts),
+            "-b",
+            "15",
+            "-t",
+            "20",
+            "SNS_Test_data_long_500.dat"
+            ])).split()
+        output = list(map(float ,output))
+        output_list.append(output)
 else:
-    for i in range(0,len(input_bits)):
+    for amount_of_bits in input_bits:
+        output = bytes.decode(check_output(
+            ["./fix_fft",
+            "-n",
+            "8",
+            "-b",
+            str(amount_of_bits),
+            "-t",
+            "20",
+            "SNS_Test_data_long_500.dat"
+            ])).split()
+        output = list(map(float ,output))
+        output_list.append(output)
+
+if(plot_error):
+    for i in range(len(twiddle_bits)):
+        output_for_plot = np.array(list(map(lambda x : x[2], output_list[i])))
+        plt.subplot(len(twiddle_bits),1,i+1)
+        plt.title("relative_error with "+str(twiddle_bits[i])+" bits")  
+        plt.plot(np.array(input_bits),output_for_plot, "bo")
+elif plot_for_bursts:
+    for i in range(len(input_bursts)):
         output_for_plot = np.array(output_list[i])
-        plt.subplot(2,2,i+1)
+        plt.subplot((len(input_bursts)+1) >> 1,2,i+1)
+        plt.title("Number of bursts "+str(input_bursts[i]))  
+        plt.plot(np.array(list(range(1,len(output_for_plot)))),output_for_plot[1:])
+else:
+    for i in range(len(input_bits)):
+        output_for_plot = np.array(output_list[i])
+        plt.subplot((len(input_bits)+1) >> 1,2,i+1)
         plt.title("Number of bits "+str(input_bits[i]))  
-        plt.plot(np.array(list(range(0,len(output_for_plot)))),output_for_plot)
+        plt.plot(np.array(list(range(1,len(output_for_plot)))),output_for_plot[1:])
 
 
 
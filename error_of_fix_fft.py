57081b648ebf1d9d28af99a31c089cbb44fb3578
bonsaiiV <dominik.riemer@udo.edu>
2022-06-08 13:45:45 +0200

    -added comandline parameters to fix_fft -removed independent fix and normalize option and made them mandatory -added result calculation for multiple bursts

--- error_of_fix_fft.py
+++ error_of_fix_fft.py
@@ -2,32 +2,84 @@ from subprocess import check_output
 import numpy as np 
 from matplotlib import pyplot as plt
 
-plot_for_bits = 0
+plot_error = 0
+plot_for_bursts = 1
 
+def print_float(s):
+    print(s+", ")
+    return float(s)
 
-if plot_for_bits:
-    input_bits = range(16, 33)
-    titles = ["absolute errors for sensor 1","absolute errors for sensor 2", "relative errors for sensor 1", "relative errors for sensor 2"]
+if plot_error:
+    input_bits = range(14, 33)
+    twiddle_bits = [12, 14, 16, 28]
+    #titles = ["absolute error"]
+elif plot_for_bursts:
+    input_bursts = range(10, 200, 30)
 else:
-    input_bits =[18, 20, 22, 24]
+    input_bits = range(16,25,2)
 output_list = []
-for amount_of_bits in input_bits:
-    output = bytes.decode(check_output(["./fix_fft", str(amount_of_bits), "SNS_Test_data_ampl.dat"])).split()
-    output = list(map(float ,output))
-    output_list.append(output)
-
-if(plot_for_bits):
-    for i in range(0,len(output)):
-        output_for_plot = np.array(list(map(lambda x : x[i], output_list)))
-        plt.subplot(2,2,i+1)
-        plt.title(titles[i])  
-        plt.plot(np.array(input_bits),output_for_plot)
+if plot_error:
+    for twiddle_index in range(len(twiddle_bits)):
+        output_list.append([])
+        for amount_of_bits in input_bits:
+            output = bytes.decode(check_output(
+                ["./fix_fft",
+                "-b",
+                str(twiddle_bits[twiddle_index]),
+                "-t",
+                str(amount_of_bits),
+                "-e",
+                "SNS_Test_data_long_500.dat"
+                ])).split()
+            output = list(map(float ,output))
+            output_list[twiddle_index].append(output)
+elif plot_for_bursts:
+    for amount_of_bursts in input_bursts:
+        output = bytes.decode(check_output(
+            ["./fix_fft",
+            "-n",
+            str(amount_of_bursts),
+            "-b",
+            "15",
+            "-t",
+            "20",
+            "SNS_Test_data_long_500.dat"
+            ])).split()
+        output = list(map(float ,output))
+        output_list.append(output)
 else:
-    for i in range(0,len(input_bits)):
+    for amount_of_bits in input_bits:
+        output = bytes.decode(check_output(
+            ["./fix_fft",
+            "-n",
+            "8",
+            "-b",
+            str(amount_of_bits),
+            "-t",
+            "20",
+            "SNS_Test_data_long_500.dat"
+            ])).split()
+        output = list(map(float ,output))
+        output_list.append(output)
+
+if(plot_error):
+    for i in range(len(twiddle_bits)):
+        output_for_plot = np.array(list(map(lambda x : x[2], output_list[i])))
+        plt.subplot(len(twiddle_bits),1,i+1)
+        plt.title("relative_error with "+str(twiddle_bits[i])+" bits")  
+        plt.plot(np.array(input_bits),output_for_plot, "bo")
+elif plot_for_bursts:
+    for i in range(len(input_bursts)):
         output_for_plot = np.array(output_list[i])
-        plt.subplot(2,2,i+1)
+        plt.subplot((len(input_bursts)+1) >> 1,2,i+1)
+        plt.title("Number of bursts "+str(input_bursts[i]))  
+        plt.plot(np.array(list(range(1,len(output_for_plot)))),output_for_plot[1:])
+else:
+    for i in range(len(input_bits)):
+        output_for_plot = np.array(output_list[i])
+        plt.subplot((len(input_bits)+1) >> 1,2,i+1)
         plt.title("Number of bits "+str(input_bits[i]))  
-        plt.plot(np.array(list(range(0,len(output_for_plot)))),output_for_plot)
+        plt.plot(np.array(list(range(1,len(output_for_plot)))),output_for_plot[1:])
 
 
 
--- fix_fft.c
+++ fix_fft.c
@@ -8,15 +8,13 @@
 #define short __int64_t
 
 int komma_pos = 0;            
-int total_bits = 32;            //used to simulate limited byte size regarding overflow and accuracy
-#define NORMALIZE 1             //0 or 1
-#define INDEPENDENT_FIX 1       //0 or 1 
+int total_bits = 32;
+int twiddle_bits = 32;
+int do_round = 0;            //used to simulate limited byte size regarding overflow and accuracy
 
 
 int n;
 int length;
-short ** sensor1_ptr;
-short ** sensor2_ptr;
 
 int had_overflow = 0;
 int error_mode = 0;
@@ -43,26 +41,23 @@ short ffix(double a){
 //creates a fixed point number from a floating point number
 //this is used for twiddle to allow different independent ranges of accurracy 
 short fix_twiddle (double a){
-    if(1-INDEPENDENT_FIX) return ffix(a);
-    double ret = a * (1 << (total_bits - 2));
+    double ret = a * (1 << (twiddle_bits - 2));
     return (short) ret;
 }
 
 //returns a floating point number from fixed point
 //this is used for twiddle to allow different independent ranges of accurracy 
 double unfix_twiddle(short a){
-    if(INDEPENDENT_FIX) return (float) a / (1 << (total_bits - 2));
-    return unfix(a);
+    return (float) a / (1 << (twiddle_bits - 2));;
 }
 
 //multiplies a twiddle value with another fixed point number
 //DONT use for another multiplication since twiddle might have a different accuracy
 short fix_mul(short a, short b){
     __int128_t c;
-    if(INDEPENDENT_FIX) c = ((__int128_t) a * (__int128_t) b) >> total_bits-3;
-    else c = ((__int128_t) a * (__int128_t) b) >> komma_pos-1;
-    b = c & 1;
-    a = (c >> 1) + b;
+    c = ((__int128_t) a * (__int128_t) b) >> twiddle_bits-(2+do_round);
+    b = c & do_round;
+    a = (c >> do_round) + b;
 
     if(abs(a) > (one << (total_bits-1))){
         //printf("There was an overflow: %f\n", unfix(a));
@@ -106,10 +101,10 @@ void butterfly(short * x, int index1, int index2, int elements_per_block, int k)
     short tmp_real = fix_mul(x[real(i2)], twiddle_real) - fix_mul(x[imag(i2)], twiddle_imag);
     short tmp_imag = fix_mul(x[real(i2)], twiddle_imag) + fix_mul(x[imag(i2)], twiddle_real);
 
-    x[real(i2)] = (x[real(i1)] - tmp_real) >> NORMALIZE;
-    x[imag(i2)] = (x[imag(i1)] - tmp_imag) >> NORMALIZE;
-    x[real(i1)] = (x[real(i1)] + tmp_real) >> NORMALIZE;
-    x[imag(i1)] = (x[imag(i1)] + tmp_imag) >> NORMALIZE;
+    x[real(i2)] = (x[real(i1)] - tmp_real) >> 1;
+    x[imag(i2)] = (x[imag(i1)] - tmp_imag) >> 1;
+    x[real(i1)] = (x[real(i1)] + tmp_real) >> 1;
+    x[imag(i1)] = (x[imag(i1)] + tmp_imag) >> 1;
 }
 
 
@@ -140,10 +135,10 @@ int radix2_fft(short * x){
 //driver code
 
 
-void read_input (const char* file_name)
+void read_input (const char* file_name, int number_of_bursts, short *(*sensor1_ptr)[], short *(*sensor2_ptr)[])
 {
-    short * sensor1 = *sensor1_ptr;
-    short * sensor2 = *sensor2_ptr;
+    short **sensor1 = *sensor1_ptr;
+    short **sensor2 = *sensor2_ptr;
     FILE *numbers;
     numbers = fopen(file_name, "r");
 
@@ -151,18 +146,22 @@ void read_input (const char* file_name)
         printf("Error reading file, try ./dft /path/to/file\n");
         exit (0);
     }
+    char str[60];
+    fgets(str, 60, numbers);
 
     int read1 = 0.0;
     int read2 = 0.0;
-    for (int i = 0; i < length; i++)
+    for(int burst = 0; burst < number_of_bursts; burst++)
     {
-        fscanf(numbers, "%d\t%d\n ", &read1, &read2);
-        sensor1[real(i)] = ifix(read1);
-        sensor1[imag(i)] = 0;
-        sensor2[real(i)] = ifix(read2);
-        sensor2[imag(i)] = 0;
+        for (int i = 0; i < length; i++)
+        {
+            fscanf(numbers, "%d\t%d\n ", &read1, &read2);
+            *(sensor1[burst]+real(i)) = ifix(read1);
+            *(sensor1[burst]+imag(i)) = 0;
+            *(sensor2[burst]+real(i)) = ifix(read2);
+            *(sensor2[burst]+imag(i)) = 0;
+        }
     }
-
     fclose(numbers);
 }
 
@@ -172,21 +171,65 @@ short create_input(int length, int i){
     return ffix(sin(M_PI * i/(length/F))+ cos(M_PI * i/(length/(F-1))));
 }
 
-
-int main(int argc, char *argv[]){
-    if (argc < 3)
-    {
-        printf("pls try \"./fix_fft #Bits input_file\"");
+void get_int(int * target, char * source, char option){
+    char * p;
+    *target = (int) strtol(source, &p, 10);
+    if(*p != '\0'){
+        if(verbose) printf("positional argument of -%c should be an int", option);
         exit(EXIT_FAILURE);
     }
-    char * p;
-    total_bits = (int) strtol(argv[1], &p, 10);
-    if(*p != '\0'){
-        printf("number of bits should be an int%c",*p);
-       exit(EXIT_FAILURE);
-    }
+}
 
-    komma_pos = total_bits - 12;
+
+int main(int argc, char *argv[]){
+    int number_of_bursts = 1;
+    char * input_file;
+
+    for(int i = 1; i < argc; i++){
+        if(*(argv[i]) == '-'){
+            
+            switch(argv[i][1]){
+                case 'n':
+                    if(i+1>=argc){
+                        if(verbose) printf("Option -n requires a positional integer argument");
+                        exit(EXIT_FAILURE);
+                    }
+                    get_int(&number_of_bursts, argv[i+1], 'n');
+                    i++;
+                    break;
+                case 'b':
+                    if(i+1>=argc){
+                        if(verbose) printf("Option -b requires a positional integer argument");
+                        exit(EXIT_FAILURE);
+                    }
+                    get_int(&total_bits, argv[i+1], 'b');
+                    i++;
+                    break;
+                case 't':
+                    if(i+1>=argc){
+                        if(verbose) printf("Option -t requires a positional integer argument");
+                        exit(EXIT_FAILURE);
+                    }
+                    get_int(&twiddle_bits, argv[i+1], 't');
+                    if(twiddle_bits < 3){
+                        if(verbose) printf("twiddle needs at least 3 bits");
+                        exit(EXIT_FAILURE);
+                    }
+                    i++;
+                    break;
+                case 'e':
+                    error_mode = 1;
+            }
+        }
+        else{
+            input_file = argv[i];
+        }
+    }
+    if(error_mode && (number_of_bursts != 1)){
+        if(verbose) printf("In error_mode number of bursts need to be 1.\nDefaulting number of bursts to 1.\n");
+        number_of_bursts = 1;
+    }
+    komma_pos = total_bits - 13;
 
     length = 1 << 15;
     int divisor = length;
@@ -194,18 +237,20 @@ int main(int argc, char *argv[]){
     while(divisor){
         divisor /= 2;
         n++;
-    }    
+    }  
+    short * sensor1[number_of_bursts];
+    short * sensor2[number_of_bursts];
+    for(int i = 0; i < number_of_bursts; i++){
+        sensor1[i] = malloc(sizeof(short)*2*length);
+        sensor2[i] = malloc(sizeof(short)*2*length);
+    }
 
-    short * sensor1 = malloc(sizeof(short)*2*length);
-    double * sensor1_gsl = malloc(sizeof(double)*2*length);
-    sensor1_ptr = &sensor1;
-    short * sensor2 = malloc(sizeof(short)*2*length);
-    double * sensor2_gsl = malloc(sizeof(double)*2*length);
-    sensor2_ptr = &sensor2;
+    
+    short * (*sensor1_ptr)[] = &sensor1;
+    short * (*sensor2_ptr)[] = &sensor2;
 
     //create inputs
-    read_input(argv[2]);
-
+    read_input(input_file, number_of_bursts, sensor1_ptr, sensor2_ptr);
     /*
     for(int i = 0; i<length; i++){
         sensor1[real(i)] = create_input(length, i);
@@ -214,74 +259,103 @@ int main(int argc, char *argv[]){
         sensor2[imag(i)] = 0;
     }*/
 
-    //convert vector representation for gsl_fft
-    for(int i = 0; i < length; i++){
-        sensor1_gsl[real(i)] = unfix(sensor1[real(i)]);
-        sensor1_gsl[imag(i)] = unfix(sensor1[imag(i)]);
-        sensor2_gsl[real(i)] = unfix(sensor2[real(i)]);
-        sensor2_gsl[imag(i)] = unfix(sensor2[imag(i)]);
+    //creating results to compare
+    double * sensor1_gsl;
+    double * sensor2_gsl;
+    if(error_mode){
+        sensor1_gsl = malloc(sizeof(double)*2*length);
+        sensor2_gsl = malloc(sizeof(double)*2*length);
+        for(int i = 0; i < length; i++){
+            sensor1_gsl[real(i)] = unfix(sensor1[0][real(i)]);
+            sensor1_gsl[imag(i)] = unfix(sensor1[0][imag(i)]);
+            sensor2_gsl[real(i)] = unfix(sensor2[0][real(i)]);
+            sensor2_gsl[imag(i)] = unfix(sensor2[0][imag(i)]);
+        }
+        gsl_fft_complex_radix2_forward(sensor1_gsl, 1,length);
+        gsl_fft_complex_radix2_forward(sensor2_gsl, 1,length);
     }
 
 
     //compute ffts
-    radix2_fft(sensor1);
-    gsl_fft_complex_radix2_forward(sensor1_gsl, 1,length);
-    radix2_fft(sensor2);
-    gsl_fft_complex_radix2_forward(sensor2_gsl, 1,length);
-
-
-    float average_abs_error1 = 0.0;
-    float average_rel_error1 = 0.0;
-    float average_abs_error2 = 0.0;
-    float average_rel_error2 = 0.0;
-    float absolute_error;
-    double * output = malloc(sizeof(double)*length/2);
-    int r_index;
-    int normalize_factor = (1 << 15) * NORMALIZE + (1-NORMALIZE);
-
-    for(int i = 0; i < length/2; i++){
-        r_index = lookup(i);
-        absolute_error = fabs(sensor1_gsl[real(i)] - normalize_factor*unfix(sensor1[real(r_index)]));
-        average_abs_error1 += absolute_error;
-        average_rel_error1 += absolute_error/fmax(1.0, fabs(sensor1_gsl[real(i)]));
-
-        absolute_error = fabs(sensor1_gsl[imag(i)] - normalize_factor*unfix(sensor1[imag(r_index)]));
-        average_abs_error1 += absolute_error;
-        average_rel_error1 += absolute_error/fmax(1.0, fabs(sensor1_gsl[imag(i)]));
-
-        absolute_error = fabs(sensor2_gsl[real(i)] - normalize_factor*unfix(sensor2[real(r_index)]));
-        average_abs_error2 += absolute_error;
-        average_rel_error2 += absolute_error/fmax(1.0, fabs(sensor2_gsl[real(i)]));
-
-        absolute_error = fabs(sensor2_gsl[imag(i)] - normalize_factor*unfix(sensor2[imag(r_index)]));
-        average_abs_error2 += absolute_error;
-        average_rel_error2 += absolute_error/fmax(1.0, fabs(sensor2_gsl[imag(i)]));
-
-
-        double tmp = unfix(sensor1[real(r_index)] - sensor2[real(r_index)]);
-        double tmp2 = unfix(sensor1[imag(r_index)]- sensor2[imag(r_index)]);
-        output[i] = tmp * tmp + tmp2 * tmp2;
+    for(int i = 0; i < number_of_bursts; i++){
+        radix2_fft(sensor1[i]);
+        radix2_fft(sensor2[i]);
     }
 
-    average_abs_error1 /= length/2;
-    average_rel_error1 /= length/2;
-    average_abs_error2 /= length/2;
-    average_rel_error2 /= length/2;
+    if(error_mode){
 
-    if(verbose){
-        printf("fix-point accuracy: %d\n", komma_pos);
-        if(had_overflow) printf("there were %d overflows in multiplications", had_overflow);
-        printf("average absolute error: %f and %f\n", average_abs_error1, average_abs_error2);
-        printf("average relative error: %f and %f\n", average_rel_error1, average_rel_error2);
-    }
-    else{
-        if(error_mode){
+        float average_abs_error1 = 0.0;
+        float average_rel_error1 = 0.0;
+        float average_abs_error2 = 0.0;
+        float average_rel_error2 = 0.0;
+        float absolute_error;
+        
+        int r_index;
+        int normalize_factor = (1 << 15);
+
+        for(int i = 0; i < length/2; i++){
+            r_index = lookup(i);
+            absolute_error = fabs(sensor1_gsl[real(i)] - normalize_factor*unfix(*(sensor1[0]+real(r_index))));
+            average_abs_error1 += absolute_error;
+            average_rel_error1 += absolute_error/fmax(1.0, fabs(sensor1_gsl[real(i)]));
+
+            absolute_error = fabs(sensor1_gsl[imag(i)] - normalize_factor*unfix(*(sensor1[0]+imag(r_index))));
+            average_abs_error1 += absolute_error;
+            average_rel_error1 += absolute_error/fmax(1.0, fabs(sensor1_gsl[imag(i)]));
+
+            absolute_error = fabs(sensor2_gsl[real(i)] - normalize_factor*unfix(*(sensor2[0]+real(r_index))));
+            average_abs_error2 += absolute_error;
+            average_rel_error2 += absolute_error/fmax(1.0, fabs(sensor2_gsl[real(i)]));
+
+            absolute_error = fabs(sensor2_gsl[imag(i)] - normalize_factor*unfix(*(sensor2[0]+imag(r_index))));
+            average_abs_error2 += absolute_error;
+            average_rel_error2 += absolute_error/fmax(1.0, fabs(sensor2_gsl[imag(i)]));
+        }
+    
+    
+
+        average_abs_error1 /= length/2;
+        average_rel_error1 /= length/2;
+        average_abs_error2 /= length/2;
+        average_rel_error2 /= length/2;
+
+        if(verbose){
+            printf("bits for input: %d\n", total_bits);
+            if(had_overflow) printf("there were %d overflows in multiplications", had_overflow);
+            printf("average absolute error: %f and %f\n", average_abs_error1, average_abs_error2);
+            printf("average relative error: %f and %f\n", average_rel_error1, average_rel_error2);
+        }
+        else
+        {
+            
             printf("%f %f %f %f\n", average_abs_error1,average_abs_error2,average_rel_error1,average_rel_error2);
         }
-        else{
-            for(int i = 0; i < length/2; i++){
-                printf("%f ",output[i]);
+    }else
+    {
+    
+        double * output = malloc(sizeof(double)*length/2);
+
+        double sum1 ;
+        double sum2;
+        int r_index;
+        for(int i = 0; i < length/2; i++){
+            r_index = lookup(i);
+            sum1 = 0.0;
+            sum2 = 0.0;
+            for(int burst = 0; burst < number_of_bursts; burst++){
+                sum1 += unfix(sensor1[burst][real(r_index)]) * unfix(sensor1[burst][real(r_index)]) + unfix(sensor1[burst][imag(r_index)])*unfix(sensor1[burst][imag(r_index)]);
+                sum2 += unfix(sensor2[burst][real(r_index)]) * unfix(sensor2[burst][real(r_index)]) + unfix(sensor2[burst][imag(r_index)])*unfix(sensor2[burst][imag(r_index)]);
+
             }
-        }       
+            sum1 = sum1 / number_of_bursts;
+            sum2 = sum2 / number_of_bursts;
+            output[i] = sum1 - sum2;
+        }
+        for(int i = 0; i < length/2; i++){
+            printf("%f ",output[i]);
+        }
+        
+               
     }
 }
+
