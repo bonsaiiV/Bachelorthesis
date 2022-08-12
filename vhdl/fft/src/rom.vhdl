eccf8e820553c931c29660d9b63da64ab2ad9653
bonsaiiV <dominik.riemer@udo.edu>
2022-08-12 11:16:59 +0200

    use generic for rom size

--- vhdl/fft/src/rom.vhdl
+++ vhdl/fft/src/rom.vhdl
@@ -14,7 +14,7 @@ entity rom is
 end rom;
 
 architecture rom_b of rom is
-    type MEMORY is array(0 to 3) of std_logic_vector(width-1 downto 0);
+    type MEMORY is array(0 to 2**length-1) of std_logic_vector(width-1 downto 0);
     signal rom_mem :MEMORY :=(
         "000000010000","110101001011", 
         "110000000000","110101110101");
