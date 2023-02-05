8b077628518ee0090d198e3631c14bb9d7bd1906
bonsaiiV <dominik.riemer@udo.edu>
2023-02-05 14:24:09 +0100

    improved tb generation, regenerated fft_tb

--- vhdl/fft/generate/gen_tb.c
+++ vhdl/fft/generate/gen_tb.c
@@ -3,7 +3,12 @@
 #include <string.h>
 //#include <math.h>
 
-char * head = 
+int fft_length;
+int bits;
+int fft_n;
+char * input_file = "";
+
+char * code_string_head = 
 "library ieee;\n"
 "use ieee.std_logic_1164.all;\n"
 "entity fft_tb is\n"
@@ -11,24 +16,28 @@ char * head =
 "architecture test of fft_tb is\n"
 "    component fft\n"
 "    generic(N : integer;\n"
-"            width :integer; \n"
-"            width_twiddle : integer);\n"
+"            width :integer);\n"
 "    port (\n"
 "       clk, fft_start: in std_logic;\n"
 "       output_valid : out std_logic;\n"
 "       inA, inB : in std_logic_vector(2*width-1 downto 0);\n"
 "       outA, outB: out std_logic_vector(2*width-1 downto 0));\n"
-"    end component;\n"
+"    end component;\n";
+
+char * code_string_signals;
+char * code_string_signals_end =
+" downto 0) := (others =>'0');\n"
 "    signal clk, fft_start : std_logic := '0';\n"
-"    signal inA, inB : std_logic_vector(37 downto 0) := (others =>'0');\n"
 "    signal output_valid : std_logic;\n"
-"    signal outA, outB : std_logic_vector(37 downto 0);\n"
-"begin\n"
+"begin\n";
+
+char * code_string_component;
+char * code_string_component_1 =
 "    fft_i: fft\n"
 "    generic map (\n"
-"        width => 19,\n"
-"        width_twiddle => 6,\n"
-"        N => 15\n"
+"        width => ";
+char * code_string_component_2 =
+"\n"
 "    )\n"
 "    port map (\n"
 "        clk => clk,\n"
@@ -45,14 +54,25 @@ char * head =
 "        wait for 1 ns;\n"
 "        clk <= '0';\n"
 "        fft_start <= '1';\n";
-char * tail = 
+
+char * code_string_inputs;
+char * clock_cycle = 
+"wait for 1 ns;\n"
+"clk <= '1';\n"
+"wait for 1 ns;\n"
+"clk <= '0';\n";
+
+char * code_string_run = 
 "while output_valid = '0' loop\n"
 "            wait for 1 ns;\n"
 "            clk <= '1';\n"
 "            wait for 1 ns;\n"
 "            clk <= '0';\n"
-"        end loop;\n"
-"        for i in 0 to 16500 loop\n"
+"        end loop;\n";
+
+char * code_string_out;
+char * code_string_out_end =
+" loop\n"
 "        wait for 1 ns;\n"
 "            clk <= '1';\n"
 "            wait for 1 ns;\n"
@@ -61,11 +81,7 @@ char * tail =
 "        wait;\n"
 "    end process;\n"
 "end test;\n";
-char * clock_cycle = 
-"wait for 1 ns;\n"
-"clk <= '1';\n"
-"wait for 1 ns;\n"
-"clk <= '0';\n";
+
 int verbose = 0;
 
 void int2bit(int n, char* out, int bits){
@@ -106,12 +122,98 @@ void get_int(int * target, char * source, char option){
         exit(EXIT_FAILURE);
     }
 }
+void gen_code_signals(){
+    int data_signal_width = 2*bits;
+    int length_dsw = snprintf(NULL, 0, "%d", data_signal_width-1); //dsw = data signal width
 
+    code_string_signals = malloc(47 + length_dsw + 120);
+    char * code_string_signals_ptr = code_string_signals;
+
+    strncpy(code_string_signals_ptr, "signal inA, inB, outA, outB : std_logic_vector(",47);
+    code_string_signals_ptr += 47;
+
+    char dsw_str[length_dsw+1];
+    snprintf(dsw_str, length_dsw+1, "%d", data_signal_width-1);
+    strncpy(code_string_signals_ptr, dsw_str, length_dsw);
+    code_string_signals_ptr += length_dsw;
+
+    strncpy(code_string_signals_ptr, code_string_signals_end, 120);
+}
+void gen_code_component(){
+    int length_width = snprintf(NULL, 0, "%d", bits-1);
+    int length_n = snprintf(NULL, 0, "%d", fft_n);
+
+    code_string_component = malloc(50 + length_width + 17 + length_n + 332);
+    char * code_string_component_ptr = code_string_component;
+
+    strncpy(code_string_component_ptr, code_string_component_1, 50);
+    code_string_component_ptr += 50;
+
+    char width_str[length_width+1];
+    snprintf(width_str, length_width+1, "%d", bits-1);
+    strncpy(code_string_component_ptr, width_str, length_width);
+    code_string_component_ptr += length_width;
+
+    strncpy(code_string_component_ptr, ",\n        N => ", 15);
+    code_string_component_ptr += 15;
+
+    char fft_n_str[length_n+1];
+    snprintf(fft_n_str, length_n+1, "%d", fft_n);
+    strncpy(code_string_component_ptr, fft_n_str, length_n);
+    code_string_component_ptr += length_n;
+
+    strncpy(code_string_component_ptr, code_string_component_2, 332);
+}
+void gen_code_inputs(){
+    int len_misc = 9;
+    int line_length = 2*bits+len_misc;
+    int len_clock_cycle = 54;
+    int total_len = 2*line_length+len_clock_cycle;
+    __int32_t * signal = malloc( sizeof(__int32_t) * fft_length);
+    read_input(input_file ,signal, bits, fft_length);
+
+    code_string_inputs = malloc(sizeof(char) * ((fft_length+10 )* total_len)+1);
+    char * code_string_inputs_ptr = code_string_inputs;
+    for (int i = 0; i < fft_length >> 1; i++){
+        strncpy(code_string_inputs_ptr , "inA<=\"",6); 
+        int2bit(0, code_string_inputs_ptr+len_misc-3, bits);
+        int2bit(signal[2*i], code_string_inputs_ptr+bits+len_misc-3, bits);
+        strncpy(code_string_inputs_ptr+2*bits+len_misc-3 , "\";\n",3);
+
+        strncpy(code_string_inputs_ptr+line_length, "inB<=\"",6); 
+        int2bit(0, code_string_inputs_ptr + line_length + len_misc-3, bits);
+        int2bit(signal[2*i+1], code_string_inputs_ptr + line_length + bits + len_misc-3, bits);
+        strncpy(code_string_inputs_ptr+line_length+2*bits+len_misc-3, "\";\n",3);
+
+        if(i<(fft_length/2)-1)strncpy(code_string_inputs_ptr+line_length+2*bits+len_misc,clock_cycle, 54);
+        else strncpy(code_string_inputs_ptr+line_length+2*bits+len_misc,"", 1);
+
+        code_string_inputs_ptr += total_len;
+    }
+
+        free(signal);
+
+}
+void gen_code_out(){
+    int fft_length_half = fft_length/2; //flh = fft_length_half
+    int length_flh = snprintf(NULL, 0, "%d", fft_length_half);
+
+    code_string_out = malloc(22 + length_flh + 164);
+    char * code_string_out_ptr = code_string_out;
+
+    strncpy(code_string_out_ptr, "        for i in 0 to ", 22);
+    code_string_out_ptr += 22;
+
+    char flh_str[length_flh];
+    snprintf(flh_str, length_flh, "%d", fft_length_half);
+    code_string_out_ptr += length_flh;
+
+    strncpy(code_string_out_ptr, code_string_out_end, 164);
+}
 int main(int argc, char * argv[]){
-    int fft_n = 15;
-    int bits = 19;
+    fft_n = 15;
+    bits = 19;
     char * output_file = "fft_tb.vhdl";
-    char * input_file = "";
     for (int i = 0; i < argc; i++)
     {
         if(*(argv[i]) == '-'){
@@ -154,42 +256,28 @@ int main(int argc, char * argv[]){
             output_file = argv[i];
         }
     }
-    int fft_length = (1<<fft_n);
-    int len_misc = 9;
-    int line_length = 2*bits+len_misc;
-    int len_clock_cycle = 54;
-    int total_len = 2*line_length+len_clock_cycle;
-    __int32_t * signal = malloc( sizeof(__int32_t) * fft_length);
-    read_input(input_file ,signal, bits, fft_length);
-    char * ret = malloc(sizeof(char) * ((fft_length+10 )* total_len)+1);
-    char * ret_ptr = ret;
+    fft_length = (1<<fft_n);
+    
     printf("%d\n", fft_length);
-    for (int i = 0; i < fft_length >> 1; i++){
-        //printf("%d\n", i);
-        strncpy(ret_ptr , "inA<=\"",6); 
-        int2bit(0, ret_ptr+len_misc-3, bits);
-        int2bit(signal[2*i], ret_ptr+bits+len_misc-3, bits);
-        strncpy(ret_ptr+2*bits+len_misc-3 , "\";\n",3);
+    gen_code_signals();
+    gen_code_component();
+    gen_code_inputs();
+    gen_code_out();
 
-        strncpy(ret_ptr+line_length, "inB<=\"",6); 
-        int2bit(0, ret_ptr + line_length + len_misc-3, bits);
-        int2bit(signal[2*i+1], ret_ptr + line_length + bits + len_misc-3, bits);
-        strncpy(ret_ptr+line_length+2*bits+len_misc-3, "\";\n",3);
-
-        if(i<(fft_length/2)-1)strncpy(ret_ptr+line_length+2*bits+len_misc,clock_cycle, 54);
-        else strncpy(ret_ptr+line_length+2*bits+len_misc,"", 1);
-
-        ret_ptr += total_len;
-    }
-    free(signal);
     FILE * outFile;
     outFile = fopen(output_file, "w");
     if(outFile == NULL){ 
         printf("failed opening output file");
         exit(EXIT_FAILURE);
     }
-    fprintf(outFile, "%s%s%s", head, ret, tail);
+    
+    fprintf(outFile, "%s%s%s%s%s%s", code_string_head, code_string_signals, code_string_component, code_string_inputs, code_string_run, code_string_out);
+
     fclose(outFile);
-    free(ret);
+
+    free(code_string_signals);
+    free(code_string_component);
+    free(code_string_inputs);
+    free(code_string_out);
     
 }
