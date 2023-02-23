2fe89efaba41cdc728ad0353d9fbca22ec3a456b
bonsaiiV <dominik.riemer@udo.edu>
2023-02-23 18:45:01 +0100

    regenerated

--- vhdl/fft/generate/grom.vhdl
+++ vhdl/fft/generate/grom.vhdl
@@ -15,7 +15,7 @@ end rom;
 architecture rom_b of rom is
     type MEMORY is array(0 to 2**length-1) of std_logic_vector(width-1 downto 0);
     signal rom_mem :MEMORY :=(
-"00000000000100000000","11010010110010110101","11000000000000000000","11010010111101001011");
+"00000000000100000000","11100111110011101100","11010010110010110101","11000101000001100001","11000000000000000000","11000101001110011111","11010010111101001011","11100111111100010100");
 begin
    process(clk)   begin       if (rising_edge(clk)) then           value <= rom_mem(to_integer(unsigned(addr)));
        end if;   end process;end rom_b;
