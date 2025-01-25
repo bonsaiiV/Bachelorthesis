e43be277b53513f559a48a3f89e3d6a8b8c16896
Bonsaiiv <dominik.riemer@udo.edu>
2025-01-25 23:18:41 +0100

    pipeline c_mult pipeline c_mult for proper DSP usage added test for c_mult

--- vhdl/fft/test/butterfly_unit/c_mult_tb.vhdl
+++ vhdl/fft/test/butterfly_unit/c_mult_tb.vhdl
@@ -6,42 +6,75 @@ entity c_mult_tb is
 end c_mult_tb;
 
 architecture test of c_mult_tb is
-    component c_mult
-        generic (width_A, width_B: integer);
-        port    (A_real, A_imag: in  signed(width_A-1 downto 0);
-                 B_real, B_imag: in  signed(width_B-1 downto 0);
-                 C_real, C_imag: out signed(width_A-1 downto 0));
-    end component;
+	component c_mult
+		generic (width_A, width_B: integer);
+		port (
+			clk: in std_logic;
+			A_real, A_imag: in  signed(width_A-1 downto 0);
+			B_real, B_imag: in  signed(width_B-1 downto 0);
 
-    signal A_real, A_imag: signed(7 downto 0);
-    signal B_real, B_imag: signed(7 downto 0);
-    signal C_real, C_imag: signed(7 downto 0);
+			result_real, result_imag: out signed(width_A-1 downto 0)
+		);
+	end component;
+
+	signal A_real, A_imag: signed(7 downto 0) := (others => '0');
+	signal B_real, B_imag: signed(7 downto 0) := (others => '0');
+	signal result_real, result_imag: signed(7 downto 0) := (others => '0');
+	signal clk : std_logic := '0';
 begin
 
-    m: c_mult
-        generic map(
-            width_A => 8,
-            width_B => 8
-        )
-        port map(
-            A_real => A_real,
-            A_imag => A_imag,
-            B_real => B_real,
-            B_imag => B_imag,
-            C_real => C_real,
-            C_imag => C_imag
-        );
+	m: c_mult
+		generic map(
+			width_A => 8,
+			width_B => 8
+		)
+		port map(
+			clk => clk,
+			A_real => A_real,
+			A_imag => A_imag,
+			B_real => B_real,
+			B_imag => B_imag,
+			result_real => result_real,
+			result_imag => result_imag
+		);
 
-    process begin
-        A_real <= "11111010"; -- -6
-        A_imag <= "11110101"; -- -11
-        B_real <= "11000000"; -- -1
-        B_imag <= "00000000";
-        wait for 10 ns;
-        B_real <= "00101101"; 
-        B_imag <= "00101101";
-        wait for 10 ns;
-        wait;
-    end process;
+	process begin
+		ASSERT result_real = 0 AND result_imag = 0
+		REPORT "initial values are wrong";
+		A_real <= "11111010"; -- -6
+		A_imag <= "11110101"; -- -11
+		B_real <= "11000000"; -- -1 in fixed point
+		B_imag <= "00000000";
+		wait for 20 ns;
+		clk <= '1';
+		wait for 10 ns;
+		clk <= '0';
+		wait for 10 ns;
+		ASSERT result_real = 0 AND result_imag = 0
+		REPORT "result is one cycle too early";
+		B_real <= "00101101"; 
+		B_imag <= "00101101";
+		clk <= '1';
+		wait for 10 ns;
+		clk <= '0';
+		wait for 10 ns;
+		ASSERT result_real = 6 AND result_imag = 11
+		REPORT "first result is wrong";
+		B_real <= "00000000"; 
+		B_imag <= "00000000";
+		clk <= '1';
+		wait for 10 ns;
+		clk <= '0';
+		wait for 10 ns;
+		--ASSERT result_real = 6 AND result_imag = 11
+		--REPORT "first result is wrong";
+		B_real <= "00000000"; 
+		B_imag <= "00000000";
+		clk <= '1';
+		wait for 10 ns;
+		clk <= '0';
+		wait for 10 ns;
+		wait;
+	end process;
 
 end test;
