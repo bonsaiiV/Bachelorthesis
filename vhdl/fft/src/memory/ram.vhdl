b06a9b5ac721309388f5aa1e073c2e29fcd2e1d6
Bonsaiiv <dominik.riemer@udo.edu>
2025-01-26 13:05:20 +0100

    rework ram ram is configured using generics ram_test made usable again shortened Makefile use vhdl 08 standard for ghdl

--- vhdl/fft/src/memory/ram.vhdl
+++ vhdl/fft/src/memory/ram.vhdl
@@ -3,40 +3,63 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity ram is
-    generic(width:integer:=16;
-            length:integer:=3);
-    port(addr_A, addr_B: in std_logic_vector(length-1 downto 0);
-         data_write_A, data_write_B: in std_logic_vector(width-1 downto 0);
-         write_enable_A, write_enable_B, clk: in std_logic;
-         data_read_A, data_read_B: out std_logic_vector(width-1 downto 0):= (others => '0'));
+	generic(
+		unknown_hardware : boolean := false;
+		word_width : integer := 16;
+		length : integer := 3
+	);
+	port(
+		clk: in std_logic;
+		addr_A, addr_B: in std_logic_vector(length-1 downto 0);
+		data_write_A, data_write_B: in std_logic_vector(word_width-1 downto 0);
+		write_enable_A, write_enable_B: in std_logic;
+		data_read_A, data_read_B: out std_logic_vector(word_width-1 downto 0):= (others => '0')
+	);
 end ram;
 
 architecture ram_b of ram is
-    type MEMORY is array(0 to 2**length-1) of std_logic_vector(width-1 downto 0);
-    signal ram_mem :MEMORY := (others => (others => '0'));
+	type MEMORY is array(0 to 2**length-1) of std_logic_vector(word_width-1 downto 0);
+	signal ram_mem :MEMORY := (others => (others => '0'));
 begin
-    process(clk)
-    begin
-        if(rising_edge(clk)) then
-            if(write_enable_A = '1') then
-                ram_mem(to_integer(unsigned(addr_A))) <= data_write_A;
-            else
-                data_read_A <= ram_mem(to_integer(unsigned(addr_A)));
-            end if;
-    --    end if;
-    --end process;
+	gen_unkown_hardware : if unknown_hardware generate
+		process(clk)
+		begin
+			if(rising_edge(clk)) then
+				if(write_enable_A = '1') then
+					ram_mem(to_integer(unsigned(addr_A))) <= data_write_A;
+				else
+					data_read_A <= ram_mem(to_integer(unsigned(addr_A)));
+				end if;
+				if(write_enable_B = '1') then
+					ram_mem(to_integer(unsigned(addr_B))) <= data_write_B;
+				else
+					data_read_B <= ram_mem(to_integer(unsigned(addr_B)));
+				end if;
+			end if;
+		end process;
+	else generate
+		process(clk)
+		begin
+			if(rising_edge(clk)) then
+				if(write_enable_A = '1') then
+					ram_mem(to_integer(unsigned(addr_A))) <= data_write_A;
+				else
+					data_read_A <= ram_mem(to_integer(unsigned(addr_A)));
+				end if;
+			end if;
+		end process;
 
-    --process(clk)
-    --begin
-    --    if(rising_edge(clk)) then
-            if(write_enable_B = '1') then
-                ram_mem(to_integer(unsigned(addr_B))) <= data_write_B;
-            else
-                data_read_B <= ram_mem(to_integer(unsigned(addr_B)));
-            end if;
-        end if;
-    end process;
-
+		process(clk)
+		begin
+			if(rising_edge(clk)) then
+				if(write_enable_B = '1') then
+					ram_mem(to_integer(unsigned(addr_B))) <= data_write_B;
+				else
+					data_read_B <= ram_mem(to_integer(unsigned(addr_B)));
+				end if;
+			end if;
+		end process;
+	end generate gen_unkown_hardware;
 
 
 end ram_b;
--- vhdl/fft/test/memory/ram_tb.vhdl
+++ vhdl/fft/test/memory/ram_tb.vhdl
@@ -7,73 +7,138 @@ entity ram_tb is
 end ram_tb;
 
 architecture test of ram_tb is
-    component ram
-        generic(width:integer;
-            length:integer);
-        port(write_addr_A, write_addr_B: in std_logic_vector(length-1 downto 0);
-             write_A, write_B: in std_logic_vector(width-1 downto 0);
-             write_enable_A, write_enable_B: in std_logic;
-             read_addr_A, read_addr_B: in std_logic_vector(length-1 downto 0);
-             read_A, read_B: out std_logic_vector(width-1 downto 0));
-    end component;
-    
-    signal write_addr_A, write_addr_B: std_logic_vector(2 downto 0);
-    signal write_A, write_B: std_logic_vector(3 downto 0);
-    signal write_enable_A, write_enable_B: std_logic := '0';
-    signal read_addr_A, read_addr_B: std_logic_vector(2 downto 0):= "000";
-    signal read_A, read_B: std_logic_vector(3 downto 0);
+	component ram
+		generic(
+			unknown_hardware : boolean := false;
+			word_width : integer := 16;
+			length : integer := 3
+		);
+		port(
+			clk: in std_logic;
+			addr_A, addr_B: in std_logic_vector(length-1 downto 0);
+			data_write_A, data_write_B: in std_logic_vector(word_width-1 downto 0);
+			write_enable_A, write_enable_B: in std_logic;
+
+			data_read_A, data_read_B: out std_logic_vector(word_width-1 downto 0):= (others => '0')
+		);
+	end component;
+	
+	signal clk: std_logic := '0';
+	signal addr_A, addr_B: std_logic_vector(2 downto 0);
+	signal write_enable_A, write_enable_B: std_logic := '0';
+	signal data_write_A, data_write_B: std_logic_vector(3 downto 0);
+
+	signal data_read_A, data_read_B: std_logic_vector(3 downto 0);
 
 begin
-    ram_i: ram
-    generic map (
-        width => 4,
-        length => 3
-    )
-    port map(
-        write_addr_A => write_addr_A,
-        write_addr_B => write_addr_B,
-        write_A => write_A, 
-        write_B => write_B,
-        write_enable_A => write_enable_A, 
-        write_enable_B => write_enable_B,
-        read_addr_A => read_addr_A, 
-        read_addr_B => read_addr_B,
-        read_A => read_A, 
-        read_B => read_B 
-    );
-    process begin
-        write_enable_A <= '0';
-        write_enable_B <= '0';
-        write_A <= "1000";
-        write_B <= "0100";
-        read_addr_A <= "000";
-        read_addr_B <= "001";
-        write_addr_A <= "000";
-        write_addr_B <= "001";
-        write_enable_A <= '1';
-        write_enable_B <= '1';
-        wait for 1 ns;
-        write_enable_A <= '0';
-        write_enable_A <= '0';
-        wait for 10 ns;
-        write_addr_A <= "010";
-        write_B <= "0010";
-        write_enable_A <= '1';
-        write_enable_B <= '1';
-        wait for 1 ns;
-        write_enable_A <= '0';
-        write_enable_A <= '0';
-        wait for 10 ns;
-        write_addr_A <= "100";
-        write_addr_B <= "011";
-        write_enable_A <= '1';
-        write_enable_B <= '1';
-        wait for 1 ns;
-        write_enable_A <= '0';
-        write_enable_A <= '0';
-        read_addr_A <= "010";
-        read_addr_B <= "100";
-        wait for 10 ns;
-        wait;
-    end process;
+	ram_i: ram
+	generic map (
+		unknown_hardware => true,
+		word_width => 4,
+		length => 3
+	)
+	port map(
+		clk => clk,
+		write_enable_A => write_enable_A, 
+		write_enable_B => write_enable_B,
+		addr_A => addr_A, 
+		addr_B => addr_B,
+		data_write_A => data_write_A, 
+		data_write_B => data_write_B,
+		data_read_A => data_read_A, 
+		data_read_B => data_read_B 
+	);
+	process begin
+		assert data_read_A = "0000" and data_read_B = "0000"
+		report "0. cycle: bad init";
+
+		data_write_A <= "1000";
+		data_write_B <= "0100";
+		addr_A <= "000";
+		addr_B <= "001";
+		write_enable_A <= '1';
+		write_enable_B <= '1';
+
+		wait for 20 ns;
+		assert data_read_A = "0000" and data_read_B = "0000"
+		report "0. cycle: read should only return values when read is enabled";
+
+		clk <= '1';
+		wait for 10 ns;
+		clk <= '0';
+		wait for 10 ns;
+
+		assert data_read_A = "0000" and data_read_B = "0000"
+		report "1. cycle: read should only return values when read is enabled";
+
+		write_enable_A <= '0';
+		write_enable_B <= '0';
+
+		clk <= '1';
+		wait for 10 ns;
+		clk <= '0';
+		wait for 10 ns;
+
+		assert data_read_A = "1000" and data_read_B = "0100"
+		report "2. cycle: read after clock cycle is wrong";
+
+		addr_A <= "001";
+		addr_B <= "000";
+
+		clk <= '1';
+		wait for 10 ns;
+		clk <= '0';
+		wait for 10 ns;
+
+		assert data_read_A = "0100" and data_read_B = "1000"
+		report "3. cycle: read after switching addresses is wrong";
+
+		addr_A <= "010";
+		addr_B <= "011";
+
+		clk <= '1';
+		wait for 10 ns;
+		clk <= '0';
+		wait for 10 ns;
+
+		assert data_read_A = "0000" and data_read_B = "0000"
+		report "4. cycle: bad read from unwritten address";
+
+		data_write_A <= "0010";
+		data_write_B <= "0001";
+		write_enable_A <= '1';
+		write_enable_B <= '1';
+
+		clk <= '1';
+		wait for 10 ns;
+		clk <= '0';
+		wait for 10 ns;
+
+		assert data_read_A = "0000" and data_read_B = "0000"
+		report "5. cycle: read value should only update when read enabled";
+
+		write_enable_A <= '0';
+		write_enable_B <= '0';
+
+		clk <= '1';
+		wait for 10 ns;
+		clk <= '0';
+		wait for 10 ns;
+
+		assert data_read_A = "0010" and data_read_B = "0001"
+		report "6. cycle: bad read from 2. address";
+
+		addr_A <= "000";
+		addr_B <= "001";
+
+		clk <= '1';
+		wait for 10 ns;
+		clk <= '0';
+		wait for 10 ns;
+
+		assert data_read_A = "1000" and data_read_B = "0100"
+		report "7. cycle: bad read from original address";
+
+		wait;
+	end process;
 end test;
