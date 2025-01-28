1f4c98ebb130f7229e879a38952b2e8570bbec41
Bonsaiiv <dominik.riemer@udo.edu>
2025-01-28 22:37:28 +0100

    add counter_tb

--- vhdl/fft/src/management_unit/counter.vhdl
+++ vhdl/fft/src/management_unit/counter.vhdl
@@ -3,36 +3,39 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity counter is
-    generic (count_width : integer; max : integer);
-    port(enable, clk : in std_logic;
-         clr : in std_logic;
-         value : out std_logic_vector(count_width-1 downto 0):= (others => '0');
-         resets : out std_logic:='0');
+	generic (
+		count_width : integer;
+		max : integer
+	);
+	port (
+		enable, clk : in std_logic;
+		clr : in std_logic;
+
+		value : out std_logic_vector(count_width-1 downto 0) := (others => '0');
+		resets : out std_logic := '0'
+	);
 end counter;
 
 architecture counter_b of counter is
 
-    signal count : unsigned(count_width-1 downto 0) := (others => '0');
-
+	signal count : unsigned(count_width-1 downto 0) := (others => '0');
 begin
-
-process(clk) begin
-    
-    if rising_edge(clk) then
-        if clr = '1' then
-            count <= (others => '0');
-            resets <= '0';
-        elsif(enable = '1') then
-            if count = max then
-                count <= (others => '0');
-                resets <= '1';
-            else
-                count <= count + 1;
-                resets <= '0';
-            end if;
-        end if;
-    end if;
-end process;
-value <= std_logic_vector(count);
+	process(clk) begin
+		if rising_edge(clk) then
+			if clr = '1' then
+				count <= (others => '0');
+				resets <= '0';
+			elsif(enable = '1') then
+				if count = max then
+					count <= (others => '0');
+					resets <= '1';
+				else
+					count <= count + 1;
+					resets <= '0';
+				end if;
+			end if;
+		end if;
+	end process;
+	value <= std_logic_vector(count);
 
 end counter_b;
--- vhdl/fft/test/management_unit/counter_tb.vhdl
+++ vhdl/fft/test/management_unit/counter_tb.vhdl
@@ -5,87 +5,111 @@ entity counter_tb is
 end counter_tb;
 
 architecture test of counter_tb is
-    component counter
-        generic (count_width : integer; max : integer);
-        port(clk : in std_logic;
-             clr : in std_logic;
-             value : out std_logic_vector(count_width-1 downto 0);
-             resets : out std_logic);
-    end component;
+	component counter
+		generic (
+			count_width : integer;
+			max : integer
+		);
+		port(
+			clk : in std_logic;
+			clr : in std_logic;
+			enable : in std_logic;
 
-    signal clk, clr, r : std_logic;
-    signal val : std_logic_vector(2 downto 0);
+			value : out std_logic_vector(count_width-1 downto 0);
+			resets : out std_logic
+		);
+	end component;
+
+	signal clk : std_logic := '0';
+	signal clr : std_logic;
+	signal value : std_logic_vector(2 downto 0);
+	signal resets : std_logic;
 begin
+	testee: counter
+		generic map (
+			count_width => 3,
+			max => 3
+		)
+		port map (
+			clk => clk,
+			enable => '1',
+			clr => clr,
+			value => value,
+			resets => resets
+		);
+	process begin
+		assert value = "001"
+		report "bad init: value"
+		severity error;
+		assert resets = '1'
+		report "bad init: resets";
 
-    count: counter
-        generic map(
-            count_width => 3,
-            max => 6
-        )
-        port map(
-            clk => clk,
-            clr => clr,
-            resets => r,
-            value => val
-        );
-    process begin
-        clr <= '1';
-        clk <= '0';
-        wait for 1 ns;
-        clk <= '1';
-        wait for 1 ns;
-        clr <= '0';
-        clk <= '0';
-        wait for 1 ns;
-        clk <= '1';
-        wait for 1 ns;
-        clk <= '0';
-        wait for 1 ns;
-        clk <= '1';
-        wait for 1 ns;
-        clk <= '0';
-        wait for 1 ns;
-        clk <= '1';
-        wait for 1 ns;
-        clk <= '0';
-        wait for 1 ns;
-        clk <= '1';
-        wait for 1 ns;
-        clk <= '0';
-        wait for 1 ns;
-        clk <= '1';
-        wait for 1 ns;
-        clk <= '0';
-        wait for 1 ns;
-        clk <= '1';
-        wait for 1 ns;
-        clk <= '0';
-        wait for 1 ns;
-        clk <= '1';
-        wait for 1 ns;
-        clk <= '0';
-        wait for 1 ns;
-        clk <= '1';
-        wait for 1 ns;
-        clk <= '0';
-        wait for 1 ns;
-        clk <= '1';
-        wait for 1 ns;
-        clr <= '1';
-        clk <= '0';
-        wait for 1 ns;
-        clk <= '1';
-        wait for 1 ns;
-        clr <= '0';
-        clk <= '0';
-        wait for 1 ns;
-        clk <= '1';
-        wait for 1 ns;
-        clk <= '0';
-        wait for 1 ns;
-        clk <= '1';
-        wait for 1 ns;
+		clr <= '1';
+		wait for 20 ns;
+		clk <= '1';
+		wait for 10 ns;
+		clk <= '0';
+		wait for 10 ns;
 
-        wait;
-    end process;
+		assert value = "000"
+		report "value should not increase while clr is active";
+
+		clr <= '0';
+		clk <= '1';
+		wait for 10 ns;
+		clk <= '0';
+		wait for 10 ns;
+
+		assert value = "001"
+		report "counter should have been 1";
+
+		clk <= '1';
+		wait for 10 ns;
+		clk <= '0';
+		wait for 10 ns;
+
+		assert value = "010"
+		report "counter should have been 2";
+
+		clk <= '1';
+		wait for 10 ns;
+		clk <= '0';
+		wait for 10 ns;
+
+		assert value = "011"
+		report "counter should have been 3";
+
+		clk <= '1';
+		wait for 10 ns;
+		clk <= '0';
+		wait for 10 ns;
+
+		assert value = "000"
+		report "counter should have been reset to 0";
+		assert resets = '1'
+		report "counter should have been signaling reset";
+
+		clk <= '1';
+		wait for 10 ns;
+		clk <= '0';
+		wait for 10 ns;
+
+		assert value = "001"
+		report "counter should start increasing again";
+		assert resets = '0'
+		report "counter should have stopped signaling reset";
+
+		clr <= '1';
+		clk <= '1';
+		wait for 10 ns;
+		clk <= '0';
+		wait for 10 ns;
+
+		assert value = "000"
+		report "counter should have been cleared";
+		assert resets = '0'
+		report "counter should have stopped signaling reset";
+
+		wait;
+	end process;
 end test;
