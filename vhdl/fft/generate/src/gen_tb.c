dfd787a8297c95f9b1cf37bad29b8c64f2dd1f93
Bonsaiiv <dominik.riemer@udo.edu>
2025-01-30 07:02:54 +0100

    improve generate

--- vhdl/fft/generate/src/gen_tb.c
+++ vhdl/fft/generate/src/gen_tb.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 #include <string.h>
 #include "common.h"
+#include "getopt.h"
 //#include <math.h>
 
-static char * code_string_head_start = 
+static char * begin_template = 
 "library ieee;\n"
 "use ieee.std_logic_1164.all;\n\n"
 "entity fft_tb is\n"
@@ -13,18 +14,11 @@ static char * code_string_head_start =
 "\tcomponent fft\n"
 "\tport (\n"
 "\t\tclk, fft_start: in std_logic;\n"
-"\t\tinA, inB : in std_logic_vector(";
-char * code_string_head_midline =
-" downto 0);\n"
+"\t\tinA, inB : in std_logic_vector(%d downto 0);\n"
 "\t\toutput_valid : out std_logic;\n"
-"\t\toutA, outB: out std_logic_vector(";
-char * code_string_head_end =
-" downto 0));\n"
-"\tend component;\n\n";
-
-char * code_string_signals;
-char * code_string_signals_end =
-" downto 0) := (others =>'0');\n"
+"\t\toutA, outB: out std_logic_vector(%d downto 0));\n"
+"\tend component;\n\n"
+"\tsignal inA, inB, outA, outB : std_logic_vector(%d downto 0) := (others =>'0');\n"
 "\tsignal clk, fft_start : std_logic := '0';\n"
 "\tsignal output_valid : std_logic;\n"
 "begin\n"
@@ -45,20 +39,30 @@ char * code_string_signals_end =
 "\t\tclk <= '0';\n"
 "\t\tfft_start <= '1';\n";
 
-char * code_string_inputs;
-char * clock_cycle = 
+char * cycle_template = 
+"\t\tinA<=\"%s\";\n"
+"\t\tinB<=\"%s\";\n"
 "\t\twait for 1 ns;\n"
 "\t\tclk <= '1';\n"
 "\t\twait for 1 ns;\n"
 "\t\tclk <= '0';\n";
 
-char * code_string_run = 
-"\twhile output_valid = '0' loop\n"
-"\t\twait for 1 ns;\n"
-"\t\tclk <= '1';\n"
-"\t\twait for 1 ns;\n"
-"\t\tclk <= '0';\n"
-"\tend loop;\n";
+char * end_template = 
+"\t\twhile output_valid = '0' loop\n"
+"\t\t\twait for 1 ns;\n"
+"\t\t\tclk <= '1';\n"
+"\t\t\twait for 1 ns;\n"
+"\t\t\tclk <= '0';\n"
+"\t\tend loop;\n"
+"\t\tfor i in 0 to %d loop\n"
+"\t\t\twait for 1 ns;\n"
+"\t\t\tclk <= '1';\n"
+"\t\t\twait for 1 ns;\n"
+"\t\t\tclk <= '0';\n"
+"\t\tend loop;\n"
+"\t\twait;\n"
+"\tend process;\n"
+"end test;\n";
 
 int verbose = 0;
 int fft_length;
@@ -66,220 +70,105 @@ int bits;
 int fft_n;
 char * input_file = "";
 
-void read_input (const char* file_name, __int32_t val[], int bits, int length)
+void read_input (const char* file_name, __int32_t ret[], int bits, int length)
 {
-	FILE *numbers;
-	numbers = fopen(file_name, "r");
+	FILE * data_file;
 
-	if (numbers == NULL){
-		printf("Error reading file, try ./gen_tb /path/to/file\n");
-		exit (0);
+	if ((data_file = fopen(file_name, "r")) == NULL){
+		perror("error reading test data");
+		exit (1);
 	}
 	char str[60];
-	fgets(str, 60, numbers);
+	fgets(str, 60, data_file);
 
-	int read1 = 0.0;
-	int read2 = 0.0;
+	int channel1 = 0;
+	int channel2 = 0;
 	for (int i = 0; i < length; i++)
 	{
-		fscanf(numbers, "%d\t%d\n ", &read1, &read2);
-		val[i] = read1<<(bits-13);
+		fscanf(data_file, "%d\t%d\n ", &channel1, &channel2);
+		ret[i] = channel1<<(bits-13);
 	}
-	fclose(numbers);
+	fclose(data_file);
 }
 
 
-char* code_string_head;
-char* code_string_head_ptr;
-void head_append(char* str, int len){
-	strncpy(code_string_head_ptr, str, len);
-	code_string_head_ptr += len;
-}
-void gen_code_head(){
-	int length_csh_start = strlen(code_string_head_start);
-	int length_csh_midline = strlen(code_string_head_midline);
-	int length_csh_end = strlen(code_string_head_end);
+char * gen_code_head(){
 	int width_signal = 2*bits-1;
-	int length_width_signal = snprintf(NULL, 0, "%d", width_signal);
+	int length_head_signal = snprintf(NULL, 0, begin_template, width_signal);
 
-	code_string_head = malloc(length_csh_start + length_width_signal + length_csh_midline + length_width_signal + length_csh_end);
-	code_string_head_ptr = code_string_head;
+	char * ret = calloc(length_head_signal, sizeof(char));
 
-	head_append(code_string_head_start, length_csh_start);
-
-	char width_str[length_width_signal+1];
-	snprintf(width_str, length_width_signal+1, "%d", width_signal);
-	head_append(width_str, length_width_signal);
-
-	head_append(code_string_head_midline, length_csh_midline);
-
-	char fft_n_str[length_width_signal+1];
-	snprintf(fft_n_str, length_width_signal+1, "%d", width_signal);
-	head_append(fft_n_str, length_width_signal);
-
-	head_append(code_string_head_end, length_csh_end);
+	snprintf(ret, length_head_signal, begin_template, width_signal, width_signal, width_signal);
+	return ret;
 }
-char * code_string_signals_ptr;
-void signals_append(char *str, int len) {
-	strncpy(code_string_signals_ptr , str, len); 
-	code_string_signals_ptr += len;
-}
-
-void gen_code_signals(){
-	int data_signal_width = 2*bits;
-	int length_dsw = snprintf(NULL, 0, "%d", data_signal_width-1); //dsw = data signal width
-	
-	char * first_signal_str = "\tsignal inA, inB, outA, outB : std_logic_vector(";
-	int length_first_signal = strlen(first_signal_str);
-
-	int length_signals_end = strlen(code_string_signals_end);
-	code_string_signals = malloc(length_first_signal + length_dsw + length_signals_end);
-	code_string_signals_ptr = code_string_signals;
-
-	signals_append(first_signal_str, length_first_signal);
-
-	char dsw_str[length_dsw+1];
-	snprintf(dsw_str, length_dsw+1, "%d", data_signal_width-1);
-	signals_append(dsw_str, length_dsw);
-
-	strncpy(code_string_signals_ptr, code_string_signals_end, length_signals_end);
-}
-char * code_string_inputs_ptr;
-void input_append(char *str, int len) {
-	strncpy(code_string_inputs_ptr , str, len); 
-	code_string_inputs_ptr += len;
-}
-void gen_code_inputs(){
-	char * assign_a_str = "\t\tinA<=\"";
-	char * assign_b_str = "\t\tinB<=\"";
-	int len_assign = strlen(assign_a_str);
-	char * line_end_str = "\";\n";
-	int len_line_end = strlen(line_end_str);
-	int len_misc = len_assign + len_line_end;
-	int line_length = 2*bits+len_misc;
-	int len_clock_cycle = strlen(clock_cycle);
-	int block_len = 2*line_length+len_clock_cycle;
+char * gen_code_inputs(){
 	__int32_t * signal = malloc( sizeof(__int32_t) * fft_length);
-	read_input(input_file ,signal, bits, fft_length);
-	int total_len = sizeof(char) * ((fft_length+10 )* block_len)+1;
-	code_string_inputs = malloc(total_len);
-	code_string_inputs_ptr = code_string_inputs;
+	read_input(input_file, signal, bits, fft_length);
+
+	char strA[bits + 1];
+	char strB[bits + 1];
+	strA[bits] = '\0';
+	strB[bits] = '\0';
+
+	int2bit(0, strA, bits);
+	int2bit(0, strB, bits);
+
+	int cycle_len = snprintf(NULL, 0, cycle_template, strA, strB);
+
+
+	int total_len = sizeof(char) * (fft_length * (cycle_len) + 1);
+
+	char * ret = malloc(total_len);
+	char * tmp = ret;
 	for (int i = 0; i < fft_length >> 1; i++){
-
-		input_append(assign_a_str, len_assign); 
-
-		int2bit(0, code_string_inputs_ptr, bits);
-		code_string_inputs_ptr += bits;
-		int2bit(signal[2*i], code_string_inputs_ptr, bits);
-		code_string_inputs_ptr += bits;
-
-		input_append(line_end_str, len_line_end);
-		input_append(assign_b_str, len_assign); 
-
-		int2bit(0, code_string_inputs_ptr, bits);
-		code_string_inputs_ptr += bits;
-		int2bit(signal[2*i+1], code_string_inputs_ptr, bits);
-		code_string_inputs_ptr += bits;
-
-		input_append("\";\n", 3);
-
-		if(i<(fft_length/2)-1) {
-			input_append(clock_cycle, len_clock_cycle);
-		}
-		else {
-			input_append("", 1);
-		}
-
+		int2bit( signal[2 * i], strA, bits);
+		int2bit( signal[2 * i + 1], strB, bits);
+		snprintf(tmp, cycle_len+1, cycle_template, strA, strB);
+		tmp += cycle_len;
 	}
-
-		free(signal);
-
+	free(signal);
+	return ret;
 }
 
-char * code_string_out_end =
-" loop\n"
-"		wait for 1 ns;\n"
-"			clk <= '1';\n"
-"			wait for 1 ns;\n"
-"			clk <= '0';\n"
-"		end loop;\n"
-"		wait;\n"
-"\tend process;\n"
-"end test;\n";
-char * code_string_out;
-char * code_string_out_ptr;
-void out_append(char * str, int len) {
-	strncpy(code_string_out_ptr , str, len); 
-	code_string_out_ptr += len;
-}
-void gen_code_out(){
-	int fft_length_half = fft_length/2; //flh = fft_length_half
-	int length_flh = snprintf(NULL, 0, "%d", fft_length_half);
-
-	char * for_loop_str = "\tfor i in 0 to ";
-	int length_for_loop_str = strlen(for_loop_str);
-	int length_code_string_out_end = strlen(code_string_out_end);
-
-	code_string_out = malloc(length_for_loop_str + length_flh + length_code_string_out_end);
-	code_string_out_ptr = code_string_out;
-
-	out_append(for_loop_str, length_for_loop_str);
-
-	char flh_str[length_flh];
-	snprintf(flh_str, length_flh+1, "%d", fft_length_half);
-	out_append(flh_str, length_flh);
-
-	out_append(code_string_out_end, length_code_string_out_end);
+char * gen_code_out(){
+	int fft_length_half = fft_length/2;
+	int length_end = snprintf(NULL, 0, end_template, fft_length_half);
+	char * ret = calloc(length_end, sizeof(char));
+	snprintf(ret, length_end, end_template, fft_length_half);
+	return ret;
 }
 int main(int argc, char * argv[]){
 	fft_n = 15;
 	bits = 19;
 	char * output_file = "fft_tb.vhdl";
 	char * config_file = 0;
-	for (int i = 0; i < argc; i++)
+	char opt;
+	while ((opt = getopt(argc, argv, "c:n:b:o:i:")) != -1)
 	{
-		if(*(argv[i]) == '-'){
-			switch(argv[i][1]){
-				case 'c':
-					if(i+1>=argc){
-						if(verbose) printf("Option -c requires a positional file argument");
-						exit(EXIT_FAILURE);
-					}
-					config_file = argv[++i];
-					break;
-				case 'n':
-					if(i+1>=argc){
-						if(verbose) printf("Option -n requires a positional integer argument");
-						exit(EXIT_FAILURE);
-					}
-					get_int(&fft_n, argv[++i], 'n', verbose);
-					break;
-				case 'b':
-					if(i+1>=argc){
-						if(verbose) printf("Option -b requires a positional integer argument");
-						exit(EXIT_FAILURE);
-					}
-					get_int(&bits, argv[++i], 'b', verbose);
-					break;
-				case 'o':
-					if(i+1>=argc){
-						if(verbose) printf("Option -o requires a positional file argument");
-						exit(EXIT_FAILURE);
-					}
-					output_file = argv[++i];
-					break;
-				case 'i':
-					if(i+1>=argc){
-						if(verbose) printf("Option -i requires a positional file argument");
-						exit(EXIT_FAILURE);
-					}
-					input_file = argv[++i];
-					break;
-			}
+		switch(opt){
+			case 'c':
+				config_file = optarg;
+			break;
+			case 'n':
+				get_int(&fft_n, optarg, 'n', verbose);
+			break;
+			case 'b':
+				get_int(&bits, optarg, 'b', verbose);
+			break;
+			case 'o':
+				output_file = optarg;
+			break;
+			case 'i':
+				input_file = optarg;
+			break;
+			default:
+				fprintf(stderr, "unknown option \"%c\"\n", opt);
+				exit(1);
+			break;
 		}
-		else{
-			output_file = argv[i];
-		}
+	}
+	if (optind < argc) {
+		output_file = argv[optind];
 	}
 	if (config_file){
 		FILE * conf_fp = fopen(config_file, "r");
@@ -305,24 +194,23 @@ int main(int argc, char * argv[]){
 	fft_length = (1<<fft_n);
 	
 
-	gen_code_head();
-	gen_code_signals();
-	gen_code_inputs();
-	gen_code_out();
+	char * code_string_head = gen_code_head();
+	char * code_string_inputs = gen_code_inputs();
+	char * code_string_end = gen_code_out();
 
 	FILE * outFile;
 	outFile = fopen(output_file, "w");
 	if(outFile == NULL){ 
-		printf("Failed opening output file.\n");
+		printf("of: %s\n", output_file);
+		perror("failed opening output file");
 		exit(EXIT_FAILURE);
 	}
 	
-	fprintf(outFile, "%s%s%s%s%s", code_string_head, code_string_signals, code_string_inputs, code_string_run, code_string_out);
+	fprintf(outFile, "%s%s%s", code_string_head, code_string_inputs, code_string_end);
 
 	fclose(outFile);
 
 	free(code_string_head);
-	free(code_string_signals);
 	free(code_string_inputs);
-	free(code_string_out);
+	free(code_string_end);
 }
--- vhdl/fft/generate/src/gen_twiddle.c
+++ vhdl/fft/generate/src/gen_twiddle.c
@@ -1,146 +1,139 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <getopt.h>
 #include "common.h"
 
 char * first_part = 
-        "library ieee;\n"
-        "use ieee.std_logic_1164.all;\n"
-        "use ieee.numeric_std.all;\n"
-        "entity rom is\n"
-        "    generic(\n"
-        "        width :integer:=12;\n"
-        "        length :integer\n"
-        "    ) ;\n"
-        "    port (\n"
-        "       clk: std_logic;\n"
-        "       addr: in std_logic_vector(length-1 downto 0);\n"
-        "       value: out std_logic_vector(width-1 downto 0) := (others => '0')\n"
-        "    );\n"
-        "end rom;\n"
-        "architecture rom_b of rom is\n"
-        "    type MEMORY is array(0 to 2**length-1) of std_logic_vector(width-1 downto 0);\n"
-        "    signal rom_mem :MEMORY :=(\n";
+		"library ieee;\n"
+		"use ieee.std_logic_1164.all;\n"
+		"use ieee.numeric_std.all;\n"
+		"entity rom is\n"
+		"	generic(\n"
+		"		width :integer:=12;\n"
+		"		length :integer\n"
+		"	) ;\n"
+		"	port (\n"
+		"	   clk: std_logic;\n"
+		"	   addr: in std_logic_vector(length-1 downto 0);\n"
+		"	   value: out std_logic_vector(width-1 downto 0) := (others => '0')\n"
+		"	);\n"
+		"end rom;\n"
+		"architecture rom_b of rom is\n"
+		"	type MEMORY is array(0 to 2**length-1) of std_logic_vector(width-1 downto 0);\n"
+		"	signal rom_mem :MEMORY :=(\n";
 char * second_part =
-        ");\n"
-        "begin\n"
-        "   process(clk)\n"
-        "   begin\n"
-        "       if (rising_edge(clk)) then\n"
-        "           value <= rom_mem(to_integer(unsigned(addr)));\n"
-        "       end if;\n"
-        "   end process;\n"
-        "end rom_b;\n";
+		");\n"
+		"begin\n"
+		"   process(clk)\n"
+		"   begin\n"
+		"	   if (rising_edge(clk)) then\n"
+		"		   value <= rom_mem(to_integer(unsigned(addr)));\n"
+		"	   end if;\n"
+		"   end process;\n"
+		"end rom_b;\n";
 
 double get_twiddle_real(int n, int i){
-    return cos(-2* M_PI*i/(1<<n));
+	return cos(-2* M_PI*i/(1<<n));
 }
 double get_twiddle_imag(int n, int i){
-    return sin(-2* M_PI*i/(1<<n));
+	return sin(-2* M_PI*i/(1<<n));
 }
 
 __int32_t ffix(double a, int komma_pos){
-    double ret = a * (1 << komma_pos);
-    return (__int32_t) ret;
+	double ret = a * (1 << komma_pos);
+	return (__int32_t) ret;
 }
 
 void generate_twiddle(int n, int bits, char * ret){
-    __int32_t real_twiddle_int;
-    __int32_t imag_twiddle_int;
-    int l_word = (2*bits+3); // 2 numbers with size = bits + leading '"', closing '"' and ','
-    for (int i = 0; i < 1 << (n-1); i++)
-    {
-        ret[i*l_word] = '"';
-        real_twiddle_int = ffix(get_twiddle_real(n,i), bits-2);
-        imag_twiddle_int = ffix(get_twiddle_imag(n,i), bits-2);
-        for (int pos = 0; pos < bits; pos++)
-        {
-            ret[i*l_word+bits-pos] = imag_twiddle_int & 1 ? '1': '0'; // index +1 for already placed '"' -1 because pos starts at 0
-            imag_twiddle_int >>= 1;
-            ret[i*l_word+2*bits-pos] = real_twiddle_int & 1 ? '1': '0'; //2* bit to target second half (real part) 
-            real_twiddle_int >>= 1;
-        }
-        ret[i*l_word+2*bits+1] = '"';
-        ret[i*l_word+2*bits+2] = ',';
-    }
-    ret[(1<<(n-1))*(2*bits+3)-1] = '\0';
-    
+	__int32_t real_twiddle_int;
+	__int32_t imag_twiddle_int;
+	int l_word = (2*bits+3); // 2 numbers with size = bits + leading '"', closing '"' and ','
+	for (int i = 0; i < 1 << (n-1); i++)
+	{
+		ret[i*l_word] = '"';
+		real_twiddle_int = ffix(get_twiddle_real(n,i), bits-2);
+		imag_twiddle_int = ffix(get_twiddle_imag(n,i), bits-2);
+		for (int pos = 0; pos < bits; pos++)
+		{
+			ret[i*l_word+bits-pos] = imag_twiddle_int & 1 + '0'; // index +1 for already placed '"' -1 because pos starts at 0
+			imag_twiddle_int >>= 1;
+			ret[i*l_word+2*bits-pos] = real_twiddle_int & 1 + '0'; //2* bit to target second half (real part) 
+			real_twiddle_int >>= 1;
+		}
+		ret[i*l_word+2*bits+1] = '"';
+		ret[i*l_word+2*bits+2] = ',';
+	}
+	ret[(1<<(n-1))*(2*bits+3)-1] = '\0';
+	
 }
 
-
-
-
+#ifdef DEBUG
+int verbose = 1;
+#else
 int verbose = 0;
+#endif
+
 
 int main(int argc, char * argv[]){
-    int fft_length = 0;
-    int bits = 0;
-    char * output_file = 0;
-    char * config_file = 0;
-    for (int i = 0; i < argc; i++)
-    {
-        if(*(argv[i]) == '-'){
-            switch(argv[i][1]){
-                case 'c':
-                    if(i+1>=argc){
-                        if(verbose) printf("Option -c requires a positional file argument"); //n is the length of a burst
-                        exit(EXIT_FAILURE);
-                    }
-					config_file = argv[++i];
-                    break;
-                case 'n':
-                    if(i+1>=argc){
-                        if(verbose) printf("Option -n requires a positional integer argument"); //n is the length of a burst
-                        exit(EXIT_FAILURE);
-                    }
-                    get_int(&fft_length, argv[i+1], 'n', verbose);
-                    i++;
-                    break;
-                case 'b':
-                    if(i+1>=argc){
-                        if(verbose) printf("Option -b requires a positional integer argument"); //b is the accuracy in bits of the twiddle factors: size = (2*b) as it is complex
-                        exit(EXIT_FAILURE);
-                    }
-                    get_int(&bits, argv[i+1], 'b', verbose);
-                    i++;
-                    break;
-            }
-        }
-        else{
-            output_file = argv[i];
-        }
-    }
+	int fft_length = 0;
+	int bits = 0;
+	char * output_file = NULL;
+	char * config_file = NULL;
+	char opt;
+	while ((opt = getopt(argc, argv, "c:n:b:o:")) != -1)
+	{
+		switch(opt){
+			case 'c':
+				config_file = optarg;
+			break;
+			case 'n':
+				get_int(&fft_length, optarg, 'n', verbose);
+			break;
+			case 'b':
+				get_int(&bits, optarg, 'b', verbose);
+			break;
+			case 'o':
+				output_file = optarg;
+			break;
+			default:
+				printf("unknown option\n");
+				exit(1);
+			break;
+		}
+	}
 	if (config_file){
 		FILE * conf_fp = fopen(config_file, "r");
 		if (!conf_fp) {
-			printf("Failed opening config file.\nMake sure it exists.\n");
+			perror("Failed opening config file");
 			exit(EXIT_FAILURE);
 		}
 		char opt = '0';
 		int val = 0;
 
-        while(fscanf(conf_fp, "-%c %d\n ", &opt, &val) != EOF){
-            switch(opt){
-                case 'n':
-                    fft_length = val;
-                    break;
-                case 'b':
-                    bits = val;
-                    break;
-            }
+		while(fscanf(conf_fp, "-%c %d\n ", &opt, &val) != EOF){
+			switch(opt){
+				case 'n':
+					fft_length = val;
+					break;
+				case 'b':
+					bits = val;
+					break;
+			}
 
 		}
+		fclose(conf_fp);
 	}
-    char * ret = malloc( sizeof(char) * (1<<(fft_length-1)) * (2*bits+3));
-    generate_twiddle(fft_length, bits, ret);
-	FILE * outFile;
-	outFile = fopen(output_file, "w");
+	char * ret = malloc( sizeof(char) * (1<<(fft_length-1)) * (2*bits+3));
+	generate_twiddle(fft_length, bits, ret);
+	FILE* outFile = fopen(output_file, "w");
 	if (outFile == NULL){
-		printf("failed to open output-file");
+		perror("failed to open output-file");
+		free(ret);
 		exit(EXIT_FAILURE);
 	}
-    fprintf(outFile, "%s%s%s",first_part,ret,second_part);
+	fprintf(outFile, "%s%s%s",first_part,ret,second_part);
 	fclose(outFile);
-    free(ret);
-    
+	free(ret);
+	
 }
